92bd30c829a61c7be9869e85d4b9917c0bd635f7
edMP <emurielpoussa@gmail.com>
2021-09-20 13:59:32 +0200

    modificacion para desafio git

--- src/main/java/com/everis/mvniniciales/App.java
+++ src/main/java/com/everis/mvniniciales/App.java
@@ -12,6 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
+    	System.out.println("Hola mundo");
+    	//desafio git
     	String a="";
     	Scanner sc= new Scanner(System.in);
     	System.out.println("inserte una cadena");
